ac4865f6a2eafcb4038dce5e5816a6dea09acae3
Geoffrey Tipton <tipton@mysecretroom.com>
2018-11-28 23:51:31 +0800

    base line

--- arguments.go
+++ arguments.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+    "flag"
+)
+
+type Params struct {
+    Lat     string
+    Long    string
+    ID      string
+    Name    string
+}
+
+func getParams() (getParams Params) {
+    //var latPtr = flag.String("lat", "", "Latitude")
+    //var lonPtr = flag.String("lon", "", "Longitude")
+    //var idPtr =  flag.String("id", "", "Force the Node ID")
+    //var namePtr =  flag.String("name", "", "Force the Node Name")
+    flag.Parse()
+
+    getParams = Params{
+        //*latPtr,
+        //*lonPtr,
+        //*idPtr,
+        //*namePtr
+    }
+
+    return
+}
